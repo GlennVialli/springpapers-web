9b0d67c5d8e0cb8cac1ade7818a9255bf3c0f2a3
GlennVialli <glenn.vialli@gmail.com>
2021-02-07 20:06:37 +0700

    routing scroll is succeed

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
-import { useRouter } from "next/router";
 
 const Layout: React.FC = ({ children }) => {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,27 +6,75 @@ import Head from "next/head";
 import Layout from "../components/layout";
 import About from "./about";
 
+type RouterPath = `/#${string}`;
+type SectionRef = {
+  ref: React.MutableRefObject<HTMLElement>;
+  routerPath: RouterPath;
+};
+
+type SectionRefMap = Record<string, SectionRef>;
+
 export default function Index() {
   const Router = useRouter();
+  const mainRef = React.useRef<HTMLDivElement>();
+  const sectionRefMap: SectionRefMap = {
+    Home: {
+      ref: React.useRef<HTMLElement>(),
+      routerPath: "/#home",
+    },
+    About: {
+      ref: React.useRef<HTMLElement>(),
+      routerPath: "/#about",
+    },
+  };
+
+  const sectionRefArr = Object.keys(sectionRefMap).map((k) => sectionRefMap[k]);
 
   React.useEffect(() => {
-    let id = Router.asPath.match(/#([a-z0-9]+)/gi);
-    if (id) {
-      // i will show the modal
-      console.log(id);
-    } else {
-      // something else
-    }
+    window.addEventListener("hashchange", (e) => {
+      foo({
+        routerPath: "/" + window.location.hash,
+        sectionRefArr,
+        scrolledRefEl: mainRef,
+      });
+    });
+  }, []);
+
+  React.useEffect(() => {
+    const routerPath = Router.asPath;
+    foo({ routerPath, sectionRefArr, scrolledRefEl: mainRef });
   }, [Router]);
 
   return (
-    <Layout>
-      <section id="home" className="section">
-        <Home />
-      </section>
-      <section id="about" className="section">
-        <About />
-      </section>
-    </Layout>
+    <div className="Main" ref={mainRef}>
+      <Layout>
+        <section ref={sectionRefMap.Home.ref} className="section">
+          <Home />
+        </section>
+        <section ref={sectionRefMap.About.ref} className="section">
+          <About />
+        </section>
+      </Layout>
+    </div>
   );
 }
+
+const foo = (params: {
+  routerPath: string;
+  sectionRefArr: SectionRef[];
+  scrolledRefEl: React.MutableRefObject<HTMLElement>;
+}) => {
+  const { routerPath, sectionRefArr, scrolledRefEl } = params;
+  const sectionRefIdx = sectionRefArr.findIndex(
+    (s) => s.routerPath == routerPath
+  );
+  if (sectionRefIdx >= 0) {
+    // i will show the modal
+    scrolledRefEl.current.scrollTo({
+      behavior: "smooth",
+      top: sectionRefArr[sectionRefIdx].ref.current.offsetTop - 100,
+    });
+  } else {
+    // something else
+  }
+};
