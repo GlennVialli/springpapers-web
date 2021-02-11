8f654ed74e02ce6c186f65b7e1b389227edd0df3
GlennVialli <glenn.vialli@gmail.com>
2021-02-11 18:00:37 +0700

    refactoring fullpage vertical

---
+++ components/fullpage-vertical.tsx
@@ -0,0 +1,149 @@
+import React from "react";
+import { useRouter } from "next/dist/client/router";
+
+type RouterPath = `/#${string}`;
+
+type FullpageVerticalSectionRef = {
+  ref: React.MutableRefObject<HTMLElement>;
+  component: JSX.Element;
+  routerPath: RouterPath;
+};
+
+type Props = {
+  sectionRefArr: FullpageVerticalSectionRef[];
+  autoScroll: boolean;
+};
+
+const FullpageVertical: React.FC<Props> = ({ sectionRefArr, autoScroll }) => {
+  const mainRef = React.useRef<HTMLDivElement>();
+  const { onScrollMain } = useFullpageVertical({
+    autoScroll,
+    sectionRefArr,
+    mainRef: mainRef.current,
+  });
+
+  return (
+    <div className="Main" ref={mainRef} onScroll={onScrollMain}>
+      {sectionRefArr.map((s) => (
+        <section ref={s.ref} key={s.routerPath} className="section">
+          {s.component}
+        </section>
+      ))}
+    </div>
+  );
+};
+
+// HOOKS
+const useFullpageVertical = (params: {
+  sectionRefArr: FullpageVerticalSectionRef[];
+  autoScroll: boolean;
+  mainRef: HTMLDivElement;
+}) => {
+  const { sectionRefArr, autoScroll, mainRef } = params;
+
+  const onFinishedScroll = () => {
+    muteOnScrollRef.current = false;
+    mainRef.style.overflowY = "scroll";
+  };
+
+  const onStartScroll = () => {
+    muteOnScrollRef.current = true;
+    mainRef.style.overflowY = "hidden";
+  };
+
+  const onHashChange = () => {
+    const idx = sectionRefArr.findIndex(
+      (s) => s.routerPath == "/" + window.location.hash
+    );
+    if (idx < 0) {
+      console.error(window.location.hash + " Hash Link not found");
+      sectionIdxRef.current = 0;
+    } else {
+      sectionIdxRef.current = idx;
+    }
+
+    const sectionRef = sectionRefArr[sectionIdxRef.current];
+    window.location.hash = sectionRef.routerPath.substring(2);
+    scrollToSection({
+      sectionRef,
+      scrolledRefEl: mainRef,
+      onStart: onStartScroll,
+      onFinished: onFinishedScroll,
+    });
+  };
+
+  const onScrollMain = () => {
+    if (muteOnScrollRef.current) return;
+    const x = sectionRefArr.reduce((p, c) => {
+      const c_diff = Math.abs(mainRef.scrollTop - c.ref.current.offsetTop);
+      const p_diff = Math.abs(mainRef.scrollTop - p.ref.current.offsetTop);
+      if (c_diff < p_diff) return c;
+      else return p;
+    });
+    if (autoScroll === false) {
+      window.onhashchange = function () {
+        window.onhashchange = onHashChange;
+      };
+    }
+
+    window.location.hash = x.routerPath.substring(2);
+  };
+
+  const Router = useRouter();
+  const muteOnScrollRef = React.useRef<boolean>(false);
+  const sectionIdxRef = React.useRef<number>(0);
+
+  React.useEffect(() => {
+    window.onhashchange = onHashChange;
+  }, []);
+
+  React.useEffect(() => {
+    const routerPath = Router.asPath;
+    if (routerPath === "/" && sectionRefArr[0]) {
+      window.location.hash = sectionRefArr[0].routerPath.substring(2);
+    }
+
+    window.onhashchange = onHashChange;
+    window.location.hash = routerPath.substring(2);
+    window.dispatchEvent(new HashChangeEvent("hashchange"));
+  }, [Router]);
+
+  return { onScrollMain };
+};
+// END OF HOOKS
+
+// EXTRA FUNCTIONS
+const scrollToSection = (params: {
+  sectionRef: FullpageVerticalSectionRef;
+  scrolledRefEl?: HTMLElement;
+  onStart?: () => void;
+  onFinished?: () => void;
+}) => {
+  const { sectionRef, scrolledRefEl, onFinished, onStart } = params;
+  if (!scrolledRefEl) return;
+
+  const position = sectionRef.ref.current.offsetTop - 100;
+  const scrollListener = (evt) => {
+    if (typeof evt === "undefined") {
+      return;
+    }
+
+    const target = evt.currentTarget;
+
+    if (target.scrollTop === position) {
+      onFinished ? onFinished() : {};
+      target.removeEventListener("scroll", scrollListener);
+    }
+  };
+
+  if (scrolledRefEl.scrollTop !== position) {
+    onStart ? onStart() : {};
+  }
+  scrolledRefEl.addEventListener("scroll", scrollListener);
+  scrolledRefEl.scrollTo({
+    behavior: "smooth",
+    top: position,
+  });
+};
+
+export default FullpageVertical;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useRouter } from "next/dist/client/router";
 import Layout, { RouterLayout } from "../components/layout";
 import About from "./about";
-import { difference } from "lodash";
+import FullpageVertical from "../components/fullpage-vertical";
 
 type SectionRef = {
   ref: React.MutableRefObject<HTMLElement>;
@@ -14,8 +14,6 @@ type SectionRef = {
 type SectionRefMap = Record<string, SectionRef>;
 
 export default function Index() {
-  const Router = useRouter();
-  const mainRef = React.useRef<HTMLDivElement>();
   const sectionRefMap: SectionRefMap = {
     Home: {
       ref: React.useRef<HTMLElement>(),
@@ -38,132 +36,15 @@ export default function Index() {
   const sectionRefArr = Object.keys(sectionRefMap).map((k) => sectionRefMap[k]);
   const routers = sectionRefArr.map((s) => s.routerLayout);
 
-  // const [muteOnScrollState, setMuteOnScrollState] = React.useState<boolean>(
-  //   false
-  // );
-  const muteOnScrollRef = React.useRef<boolean>(false);
-  const sectionIdxRef = React.useRef<number>(0);
-
-  const onFinishedScroll = () => {
-    // setMuteOnScrollState(false);
-    muteOnScrollRef.current = false;
-    mainRef.current.style.overflowY = "scroll";
-  };
-
-  const onStartScroll = () => {
-    // setMuteOnScrollState(true);
-    muteOnScrollRef.current = true;
-    mainRef.current.style.overflowY = "hidden";
-  };
-
-  const doScroll = () => {
-    console.log("kontol");
-    const idx = sectionRefArr.findIndex(
-      (s) => s.routerLayout.routerPath == "/" + window.location.hash
-    );
-    if (idx < 0) {
-      console.error(window.location.hash + " Hash Link not found");
-      sectionIdxRef.current = 0;
-    } else {
-      sectionIdxRef.current = idx;
-    }
-
-    const sectionRef = sectionRefArr[sectionIdxRef.current];
-    window.location.hash = sectionRef.routerLayout.routerPath.substring(2);
-    scrollToSection({
-      sectionRef,
-      scrolledRefEl: mainRef,
-      onStart: onStartScroll,
-      onFinished: onFinishedScroll,
-    });
-  };
-
-  React.useEffect(() => {
-    // console.log("kontol");
-    window.onhashchange = doScroll;
-  }, []);
-
-  React.useEffect(() => {
-    const routerPath = Router.asPath;
-    if (routerPath === "/" && sectionRefArr[0]) {
-      window.location.hash = sectionRefArr[0].routerLayout.routerPath.substring(
-        2
-      );
-    }
-
-    window.onhashchange = doScroll;
-    window.location.hash = routerPath.substring(2);
-    // doScroll();
-    window.dispatchEvent(new HashChangeEvent("hashchange"));
-  }, [Router]);
-
   return (
     <Layout routers={routers}>
-      <div
-        className="Main"
-        ref={mainRef}
-        onScroll={(e) => {
-          if (muteOnScrollRef.current) return;
-          const x = sectionRefArr.reduce((p, c) => {
-            const c_diff = Math.abs(
-              mainRef.current.scrollTop - c.ref.current.offsetTop
-            );
-            const p_diff = Math.abs(
-              mainRef.current.scrollTop - p.ref.current.offsetTop
-            );
-            if (c_diff < p_diff) return c;
-            else return p;
-          });
-          window.onhashchange = function () {
-            // console.log("tol");
-            window.onhashchange = doScroll;
-          };
-          window.location.hash = x.routerLayout.routerPath.substring(2);
-        }}
-      >
-        {sectionRefArr.map((s) => (
-          <section
-            ref={s.ref}
-            key={s.routerLayout.routerPath}
-            className="section"
-          >
-            {s.component}
-          </section>
-        ))}
-      </div>
+      <FullpageVertical
+        autoScroll={false}
+        sectionRefArr={sectionRefArr.map((s) => ({
+          routerPath: s.routerLayout.routerPath,
+          ...s,
+        }))}
+      />
     </Layout>
   );
 }
-
-// EXTRA FUNCTIONS
-const scrollToSection = (params: {
-  sectionRef: SectionRef;
-  scrolledRefEl: React.MutableRefObject<HTMLElement>;
-  onStart?: () => void;
-  onFinished?: () => void;
-}) => {
-  const { sectionRef, scrolledRefEl, onFinished, onStart } = params;
-
-  const position = sectionRef.ref.current.offsetTop - 100;
-  const scrollListener = (evt) => {
-    if (typeof evt === "undefined") {
-      return;
-    }
-
-    const target = evt.currentTarget;
-
-    if (target.scrollTop === position) {
-      onFinished ? onFinished() : {};
-      target.removeEventListener("scroll", scrollListener);
-    }
-  };
-
-  if (scrolledRefEl.current.scrollTop !== position) {
-    onStart ? onStart() : {};
-  }
-  scrolledRefEl.current.addEventListener("scroll", scrollListener);
-  scrolledRefEl.current.scrollTo({
-    behavior: "smooth",
-    top: position,
-  });
-};
