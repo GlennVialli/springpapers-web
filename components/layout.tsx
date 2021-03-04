6025ddd956ad958c08fe16bd6689b9509697cc5f
GlennVialli <glenn.vialli@gmail.com>
2021-03-04 12:11:16 +0700

    Header bar

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import { Table } from "@material-ui/core";
+import { useRouter } from "next/dist/client/router";
 
 type RouterPath = `/#${string}`;
 export type RouterLayout = {
@@ -16,6 +17,38 @@ type Props = {
 };
 
 const Layout: React.FC<Props> = ({ children, routers }) => {
+  const Router = useRouter();
+  const [tabValue, setTabValue] = React.useState(0);
+
+  // NEED TO MAKE A STANDALONE HOOKS AND IMPLEMENT TO FULLPAGE-HORIZONTAL-HASHROUTE AND FULLPAGE-VERTICAL-HASROUTE
+  const onHashChange = () => {
+    const idx = routers.findIndex(
+      (s) => s.routerPath == "/" + window.location.hash
+    );
+    if (idx < 0) {
+      console.error(window.location.hash + " Hash Link not found");
+      setTabValue(0);
+    } else {
+      setTabValue(idx);
+    }
+  };
+
+  React.useEffect(() => {
+    window.onhashchange = onHashChange;
+  }, []);
+
+  React.useEffect(() => {
+    const routerPath = Router.asPath;
+    if (routerPath === "/" && routers[0]) {
+      window.location.hash = routers[0].routerPath.substring(2);
+    }
+
+    window.onhashchange = onHashChange;
+    window.location.hash = routerPath.substring(2);
+    window.dispatchEvent(new HashChangeEvent("hashchange"));
+  }, [Router]);
+  // ******************************************************************************************************************
+
   return (
     <>
       <Head>
@@ -25,7 +58,7 @@ const Layout: React.FC<Props> = ({ children, routers }) => {
       </Head>
       <div className="layoutContainer">
         <div className="header">
-          <Tabs>
+          <Tabs value={tabValue}>
             {routers.map((r) => (
               <Link href={r.routerPath} shallow={true} key={r.routerPath}>
                 <Tab label={r.labelRoute} />
