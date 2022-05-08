7321b27939c16c136c106d30f9850600cdd16fa3
GlennVialli <glenn.vialli@gmail.com>
2022-05-08 19:02:53 +0700

    searchbar

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 import React from "react";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import { Table } from "@material-ui/core";
+import { InputBase, Table } from "@material-ui/core";
 import { useRouter } from "next/dist/client/router";
 import { useHashRoute } from "../hooks/useHashRoute";
 import { useWindowSize } from "../hooks/useWindowSize";
-import zIndex from "@material-ui/core/styles/zIndex";
+import SearchIcon from "@material-ui/icons/Search";
 
 type RouterPath = `/#${string}`;
 export type RouterLayout = {
@@ -46,7 +46,27 @@ const Layout: React.FC<Props> = ({ children, routers }) => {
             <img src="/img/SP-Image1.png" />
           </div>
           <div className="topHeader zigzag-border-bottom">
-            <div className="search-bar"></div>
+            <div className="search-bar">
+              <div className="search-icon">
+                <svg
+                  className="MuiSvgIcon-root"
+                  focusable="false"
+                  viewBox="0 0 512 512"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M 191 40.633 C 181.848 41.767, 165.965 45.377, 156.529 48.467 C 94.462 68.799, 48.483 125.091, 40.953 189.965 C 39.119 205.768, 40.224 231.616, 43.348 246 C 58.287 314.782, 111.887 366.778, 180.569 379.115 C 194.795 381.670, 222.709 382.187, 235.500 380.132 C 266.497 375.152, 295.079 362.754, 317.236 344.679 C 320.391 342.106, 323.317 340, 323.739 340 C 324.161 340, 353.754 369.257, 389.503 405.015 C 456.075 471.606, 457.299 472.706, 463.226 471.277 C 466.449 470.500, 470.716 466.269, 471.286 463.283 C 472.478 457.049, 471.572 456.040, 405.015 389.503 C 369.257 353.754, 340 324.161, 340 323.739 C 340 323.317, 342.106 320.391, 344.679 317.236 C 362.756 295.077, 375.166 266.464, 380.129 235.500 C 382.253 222.246, 381.754 194.628, 379.134 180.500 C 367.738 119.039, 324.268 68.329, 266.500 49.103 C 246.198 42.347, 237.242 40.867, 214.500 40.512 C 203.500 40.340, 192.925 40.394, 191 40.633 M 189.500 63.516 C 132.262 72.779, 86.599 111.717, 69.171 166.126 C 63.822 182.823, 62.550 191.601, 62.595 211.500 C 62.654 237.907, 66.446 254.159, 77.993 277.500 C 97.690 317.316, 134.443 346.121, 179 356.663 C 194.547 360.342, 219.019 360.998, 235.506 358.179 C 300.517 347.061, 349.699 296.089, 359.079 230.108 C 360.598 219.421, 359.697 192.212, 357.486 182 C 353.944 165.638, 345.392 144.386, 337.203 131.598 C 315.365 97.495, 280.554 73.522, 240.500 65.005 C 227.254 62.188, 202.224 61.457, 189.500 63.516"
+                    stroke="none"
+                    fill="#040404"
+                    fill-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <InputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
             <div className="title">
               <img src="/img/SP-Logo.png" />
             </div>
